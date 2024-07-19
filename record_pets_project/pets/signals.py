5dfd8ba3767bc1f577f176dc915774a6988f1098
Anna Pestova <pestova.an9@gmail.com>
2024-07-19 19:19:55 +0300

    Добавлено: удаление файла при удалении объекта фото из БД

---
+++ record_pets_project/pets/signals.py
@@ -0,0 +1,18 @@
+import os
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+
+from .models import PetPhoto
+
+
+@receiver(post_delete, sender=PetPhoto)
+def delete_photo_file(sender, instance, **kwargs):
+    """
+    Сигнал, отслеживает удаление объектов из модели PetPhoto
+    и удаляет файл фотографии из папки.
+    """
+
+    url = instance.photo.path
+    if os.path.exists(url):
+        os.remove(url)
+    print('Фото удалено')
